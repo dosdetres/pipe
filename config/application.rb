0a4bfa8101bf86f2e979e1b99083af49c2fb989c
Alejandro Reyes <alex_reyes@live.com.mx>
2015-07-28 04:02:55 -0500

    se agrego archivo de internacionalizacion para usar mensajes en español para devise

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Pipe
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es
     config.to_prepare do
       Devise::SessionsController.layout "login.html.erb"
       Devise::RegistrationsController.layout "application.html.erb"
