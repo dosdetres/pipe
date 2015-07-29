4f20d60f330cf0a5779a12335f3cb6bc3192575a
Alejandro Reyes <alex_reyes@live.com.mx>
2015-07-28 21:59:17 -0500

    se agregaron validaciones a las empresas

--- app/models/customer_company.rb
+++ app/models/customer_company.rb
@@ -3,7 +3,15 @@ class CustomerCompany < ActiveRecord::Base
   has_many :empresas, :class_name=>"CustomerCompany", :foreign_key => "customer_company_id"
   has_many :users
 
-  validates :empresa_cliente , presence: { message: "La empresa es requerida"}
+  validates :empresa_cliente , presence: {  message: "requerida."}
+
+  HUMANIZED_ATTRIBUTES = {
+      :empresa_cliente => 'Empresa'
+  }
+
+  def self.human_attribute_name(attr, options = {})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
 
 
   def consorcio_desc
