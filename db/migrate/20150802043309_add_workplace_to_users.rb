c23dfa16cc0c632cbbc8804a5cd1d7c428311e21
Alejandro Reyes <alex_reyes@live.com.mx>
2015-08-02 01:58:17 -0500

    se agrego migracion con referencia a workplaces

---
+++ db/migrate/20150802043309_add_workplace_to_users.rb
@@ -0,0 +1,10 @@
+class AddWorkplaceToUsers < ActiveRecord::Migration
+  def self.up
+    remove_column :users, :puesto_id
+    add_reference :users, :workplace, index: true
+  end
+
+  def self.down
+    remove_column :users, :workplace_id
+  end
+end
